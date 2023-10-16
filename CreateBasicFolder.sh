1105a17a0ac7e2510acf9136a821857a82806ee5
qiliang.huang <qiliang.huang@electrifai.com>
2023-10-16 09:49:53 +0800

    Init

---
+++ CreateBasicFolder.sh
@@ -0,0 +1,22 @@
+mkdir input
+mkdir output
+mkdir supplement_data
+mkdir tmp
+mkdir library
+mkdir packages
+
+touch ./input/info.md
+touch ./output/info.md
+touch ./supplement_data/info.md
+touch ./tmp/info.md
+touch ./library/info.md
+touch ./packages/info.md
+
+
+touch README.md
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Utils_Initialization
+# Utils Initialization
+
+## Command
+
+```shell
+# Run CreateBasicFolder.sh to initial the project
+. CreateBasicFolder.sh
+
+```
+
